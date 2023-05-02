f5f7f7f47332dfd474d165408e1402728893af1d
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2023-05-02 15:39:52 +0900

    feat: add Purchase model AND purchase_item table(Pivot) with seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Purchase;
+use App\Models\Customer;
+use App\Models\Item;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -25,8 +29,21 @@ class DatabaseSeeder extends Seeder
             ItemSeeder::class,
         ]);
 
-        \App\Models\Customer::factory(1000)->create();        
-        \App\Models\Purchase::factory(100)->create();        
+        Customer::factory(1000)->create();
 
+        //　Purchaseを登録時に中間テーブルにも同時に登録する。
+        // (1件の購入情報に1件〜3件の商品情報を登録とする)
+        // each ・・・1件ずつ処理
+        // attach ・・・中間テーブルに情報登録
+        // 外部キー以外で中間テーブルに情報追加するには第２引数に書く
+        $items = Item::all();
+        Purchase::factory(100)->create()
+            ->each(function (Purchase $purchase) use ($items) {
+                $purchase->items()->attach(
+                    $items->random(rand(1, 3))->pluck('id')->toArray(),
+                    // 1〜3個のitemをpurchaseにランダムに紐付け
+                    ['quantity' => rand(1, 5)]
+                );
+            });
     }
 }
